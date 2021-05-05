7fe78e569040f4c980f59ec33860e63e7bd8cc2c
rootStead <83608821+rootStead@users.noreply.github.com>
2021-05-04 21:57:18 -0400

    Add files via upload

---
+++ asset/script/main.js
@@ -0,0 +1,10 @@
+var cursor = true;
+        var speed = 250;        setInterval(() => {
+          if(cursor) {
+            document.getElementById('cursor').style.opacity = 0;
+            cursor = false;
+          }else {
+            document.getElementById('cursor').style.opacity = 1;
+            cursor = true;
+          }
+        }, speed);
